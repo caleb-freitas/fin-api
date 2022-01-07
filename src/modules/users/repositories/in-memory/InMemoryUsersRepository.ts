06525a6cbfb380b65d14102a2cb381934911129b
caleb-freitas <calebfreitas@tutanota.com>
2022-01-07 18:36:11 -0300

    feat: change the export mode

--- src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
+++ src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
@@ -3,15 +3,15 @@ import { User } from "../../entities/User";
 import { ICreateUserDTO } from "../../useCases/createUser/ICreateUserDTO";
 import { IUsersRepository } from "../IUsersRepository";
 
-export class InMemoryUsersRepository implements IUsersRepository {
+class InMemoryUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
   async findByEmail(email: string): Promise<User | undefined> {
-    return this.users.find(user => user.email === email);
+    return this.users.find((user) => user.email === email);
   }
 
   async findById(user_id: string): Promise<User | undefined> {
-    return this.users.find(user => user.id === user_id);
+    return this.users.find((user) => user.id === user_id);
   }
 
   async create(data: ICreateUserDTO): Promise<User> {
@@ -21,3 +21,5 @@ export class InMemoryUsersRepository implements IUsersRepository {
     return user;
   }
 }
+
+export { InMemoryUsersRepository };
