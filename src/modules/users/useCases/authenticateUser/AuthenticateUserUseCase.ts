a3f634fa60e1c64c11736cef6631a3a6a5299d13
caleb-freitas <calebfreitas@tutanota.com>
2022-01-07 21:59:51 -0300

    feat: change exportation mode

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,8 +1,8 @@
 import { inject, injectable } from "tsyringe";
-import { compare } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
-import authConfig from '../../../../config/auth';
+import authConfig from "../../../../config/auth";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { IAuthenticateUserResponseDTO } from "./IAuthenticateUserResponseDTO";
@@ -14,16 +14,19 @@ interface IRequest {
 }
 
 @injectable()
-export class AuthenticateUserUseCase {
+class AuthenticateUserUseCase {
   constructor(
-    @inject('UsersRepository')
-    private usersRepository: IUsersRepository,
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
-  async execute({ email, password }: IRequest): Promise<IAuthenticateUserResponseDTO> {
+  async execute({
+    email,
+    password,
+  }: IRequest): Promise<IAuthenticateUserResponseDTO> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if(!user) {
+    if (!user) {
       throw new IncorrectEmailOrPasswordError();
     }
 
@@ -33,7 +36,9 @@ export class AuthenticateUserUseCase {
       throw new IncorrectEmailOrPasswordError();
     }
 
-    const { secret, expiresIn } = authConfig.jwt;
+    // const { secret, expiresIn } = authConfig.jwt;
+    const secret = "secretword";
+    const expiresIn = "1d";
 
     const token = sign({ user }, secret, {
       subject: user.id,
@@ -44,9 +49,11 @@ export class AuthenticateUserUseCase {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
       },
-      token
-    }
+      token,
+    };
   }
 }
+
+export { AuthenticateUserUseCase };
